023562dcc2aeb2f6fb64df4dec950c0f01c64268
Lara Thompson <larathompson288@gmail.com>
2020-07-28 12:59:58 +0100

    hi

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,7 @@ import { Splashscreen } from './components/SplashScreen'
 import { Register } from './components/Register'
 import { Login } from './components/Login'
 import { Users } from './components/GetUsers'
+import { Preferences } from './components/Preferences'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/pavlova' component={Users} />
+          <Route exact path = '/preferences' component={Preferences} />
         </Switch>
       </main>
     </HashRouter>
