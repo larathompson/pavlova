e593e0ea18cd1203ca0b2f1953034b53dabd4c08
Nick Hayes <nhayes559@gmail.com>
2020-07-30 19:31:12 +0100

    Update config again

--- backend/environment/config.py
+++ backend/environment/config.py
@@ -1,4 +1,4 @@
 import os
 
-db_URI = os.getenv('postgres://localhost:5432/datingdb')
-secret = os.getenv('This is a very secret string only I know')
+db_URI = os.getenv('DATABASE_URL', 'postgres://localhost:5432/datingdb')
+secret = os.getenv('SECRET', 'This is a very secret string only I know')
